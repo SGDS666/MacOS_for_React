d19b00054d5a7784d694bd3c37f46282b52fe6cc
SGDS <1226931269@qq.com>
2022-06-05 23:41:06 +0800

    dock完善

--- src/components/程序坞/index.tsx
+++ src/components/程序坞/index.tsx
@@ -1,25 +1,21 @@
 /* eslint-disable react/jsx-pascal-case */
 import React, {  useEffect, useRef, useState } from 'react';
 import style from './index.module.scss';
-const 程序:React.FC<{name?:any}> = ({name}) => {
+import { 默认程序 } from './程序配置';
+
+const 程序:React.FC<{name?:any,icon:string}> = ({name,icon}) => {
     return (
         <div className={style.app}>
-            {name}
+            <div className={style.icon}>
+                <img src={icon} alt={""} />
+                <div className={style.tip}>{name}</div>
+            </div>
         </div>
     )
 }
-const 测试程序 = [
-    {name:"1"},
-    {name:"2"},
-    {name:"3"},
-    {name:"4"},
-    {name:"5"},
-    {name:"6"},
-    {name:"7"},
-    {name:"8"},
-]
+
 const 程序坞 = () => {
-    const [apps,] = useState(测试程序)
+    const [apps,] = useState(默认程序)
     const dockref:any = useRef()
     useEffect(()=>{
         const dock:HTMLDivElement = dockref.current
@@ -30,8 +26,8 @@ const 程序坞 = () => {
             console.log('进入');
             applist.forEach((app:HTMLDivElement) => {
                 setTimeout(() => {
-                    app.style.transition = 'all 0s ease'
-                }, 10);
+                    app.style.transition = 'all 0s '
+                }, 300);
             })
             
         }
@@ -43,20 +39,31 @@ const 程序坞 = () => {
                 const appcenter = app.offsetLeft + app.offsetWidth / 2
                 const appgap = Math.abs(mousex-appcenter)
                 // app.innerHTML = `gap:${appgap} size:${50 + 100 - appgap} top:${120 - appgap}`
-                if(appgap<200){
+                if(appgap<300){
                     // 尺寸 = 基础50 + 100/除以距离  越小越大
-                    const size = 50 + 120 - appgap/1.5
-                    const top = 120 - appgap/1.5
-                    app.style.width =`${size}px` 
-                    app.style.height = `${size}px` 
-                    if(top>0){
+                    const size = 50 + 120 - appgap/1.3
+                    const top = 100 - appgap/1.4
+
+                    if(size>=50){
+                        app.style.width = `${size}px`
+                        app.style.height = `${size}px`
+                    }
+                    // app.style.left = `${left}px`
+                    // if(scale>1){
+                    //     app.style.transform = `scale(${size*0.018})`
+                    // }
+                    if(top>=0){
                         app.style.top = `${-top}px`
+                        
                     }
                     
                 }else{
-                    app.style.width =`` 
-                    app.style.height = `` 
+                    app.style.width = ''
+                    app.style.height = ''
                     app.style.top = ''
+                    app.style.margin = ``
+                    app.style.transform = ''
+                    app.style.left = ''
                 }
                 
             })
@@ -65,23 +72,32 @@ const 程序坞 = () => {
         
         dock.onmouseleave = e => {
             applist.forEach((app:HTMLDivElement)=>{
-                    app.style.width =`` 
-                    app.style.height = `` 
                     app.style.top = ''
-                    app.style.transition = ''
+                    app.style.width = ''
+                    app.style.height = ''
+                    setTimeout(() => {
+                        app.style.transition = ' '
+                    }, 300);
+                    app.style.margin = ``
+                    // app.style.transform = ''
+                    
+                    
             })
         }
     },[apps])
     return (
         <div className={style.box} ref={dockref} >
             <div className={style.space}></div>
+            <div className={style.apps}>
+                {apps.map((app,index)=>(
+                    <程序 
+                    key={`applink${index}`} 
+                    name={app.name} 
+                    icon={app.icon}
+                    />
+                ))}
+            </div>
             
-            {apps.map((app,index)=>(
-                <程序 
-                key={`applink${index}`} 
-                name={app.name} 
-                />
-            ))}
             
         </div>
     )
---
+++ src/components/程序坞/程序配置.ts
@@ -0,0 +1,29 @@
+import 启动台icon from '../../image/启动台.png'
+import 访达icon from '../../image/Finder.png'
+import 设置icon from '../../image/系统偏好设置.png'
+import safarlicon from '../../image/safarl.png'
+import AppStoreIcon from '../../image/appstore.png'
+import 地图icon from '../../image/地图icon.png'
+import 活动监视器 from '../../image/活动监视器.png'
+import 终端icon from '../../image/终端.png'
+import 照片icon from '../../image/照片.png'
+import 提醒icon from '../../image/提醒.png'
+import 日历icon from '../../image/日历.png'
+import 时钟icon from '../../image/时钟.png'
+import 废纸篓icon from '../../image/废纸篓空明.png'
+
+export const 默认程序 = [
+    {name:"访达",icon:访达icon},
+    {name:"设置",icon:设置icon},
+    {name:"启动台",icon:启动台icon},
+    {name:"Safarl 浏览器",icon:safarlicon},
+    {name:"App Store",icon:AppStoreIcon},
+    {name:"地图",icon:地图icon},
+    {name:"活动监视器",icon:活动监视器},
+    {name:"终端",icon:终端icon},
+    {name:"照片",icon:照片icon},
+    {name:"提醒",icon:提醒icon},
+    {name:"日历",icon:日历icon},
+    {name:"时钟",icon:时钟icon},
+    {name:"废纸篓",icon:废纸篓icon},
+]
